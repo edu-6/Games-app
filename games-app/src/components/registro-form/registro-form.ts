fdff6939426ede88212ae67460fd22140dcecfb8
edu-6 <edudiazdl326@gmail.com>
2025-12-12 16:53:21 -0600

    Agregación de imagen a fomrulario de registro

--- games-app/src/components/registro-form/registro-form.ts
+++ games-app/src/components/registro-form/registro-form.ts
@@ -6,5 +6,27 @@ import { Component } from '@angular/core';
 })
 
 export class RegistroForm {
+  fotoSeleccionada: File | null = null;
+  hayArchivoCargado: boolean = false;
+  urlTemporal: String = "url";
+
+
+  AlCargarArchivo(event: any): void {
+    const archivos = event.target.files;
+    if (archivos && archivos.length > 0) {
+      //Seguridad para solo permitir imagenes
+      if (!archivos[0].type.startsWith('image/')) {
+        alert('Solo se permiten imágenes');
+        event.target.value = ''; // limpiar el contenedor
+        return;
+      }
+
+      this.fotoSeleccionada = archivos[0];
+      this.hayArchivoCargado = true;
+      this.urlTemporal = URL.createObjectURL(archivos[0]);
+
+      
+    }
+  }
 
 }
